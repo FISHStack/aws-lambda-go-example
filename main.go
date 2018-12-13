6317b8e8afc0e82c0cc0bcb16c63eea3d055a19e
fish <gcslyp@gmail.com>
2018-12-13 23:53:09 +0800

    add print req header

--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"fmt"
 )
 
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	for key,value := range request.Headers {
+		fmt.Println(key+":"+value)
+	}
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       "Hello AWS Lambda and Netlify",
